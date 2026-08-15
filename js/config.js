432a72ff255bf0873c830a483d34467e70d2f856
ahneth <ahneth@gmail.com>
2026-08-15 20:18:58 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,14 +1,13 @@
 // js/config.js
 export const FRED_CONFIG = {
-  BASE_URL: 'https://api.stlouisfed.org/fred/series/observations',
   API_KEY: localStorage.getItem('FRED_API_KEY') || '',
-  BENCHMARK: 'SP500',
   SERIES: {
     VIX: 'VIXCLS',
     YIELD_CURVE: 'T10Y2Y',
     CREDIT_SPREAD: 'BAMLH0A0HYM2',
-    SAHM_RULE: 'SAHMCURRENT',
-    NFCI: 'NFCI',
-    BREADTH: 'NASDAQCOM' // Unrestricted FRED equity series
-  }
+    SAHM_RULE: 'SAHMREALTIME',
+    NFCI: 'ANFCI',
+    STLFSI: 'STLFSI4' // Replaces BREADTH
+  },
+  BENCHMARK: 'SP500'
 };
