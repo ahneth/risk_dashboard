c34c5c0aa7ee75867d1f417d300348237a7f1222
ahneth <ahneth@gmail.com>
2026-08-16 09:37:38 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,5 +5,9 @@ export const SERIES_IDS = {
   credit_spread: 'BAMLH0A0HYM2',
   sahm_rule: 'SAHMREALTIME',
   nfci: 'NFCI',
-  stlfsi: 'STLFSI4'
+  stlfsi: 'STLFSI4',
+  ted_spread: 'TEDRATE',
+  bbb_spread: 'BAMLC0A4CBBB',
+  fed_liquidity: 'WALCL',
+  consumer_sentiment: 'UMCSENT'
 };
